325877bd572f228ad33bfa66664e18c744585986
mousehuang <mousehuangcoca@163.com>
2018-05-29 23:07:40 +0800

    Add testing classes.

---
+++ src/UnitTests/ContractTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JingTum.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ContractTests
+    {
+        [TestMethod]
+        public void TestDeployContractTx()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCallContractTx()
+        {
+
+        }
+    }
+}
---
+++ src/UnitTests/TransactionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JingTum.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TransactionTests
+    {
+        [TestMethod]
+        public void TestPaymentTx()
+        {
+
+        }
+    }
+}
