f6b7a6bd94575eb21fa3ab7421cabb5c2b16431b
mousehuang <mousehuangcoca@163.com>
2018-05-29 22:06:29 +0800

    Add Errors class.

---
+++ src/UnitTests/ErrorsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JingTum.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ErrorsTests
+    {
+        [TestMethod]
+        [DataRow("NONE", null)]
+        [DataRow(null, null)]
+        [DataRow("tecCLAIM", "Fee claimed. Sequence used. No action.")]
+        [DataRow("tecNO_REGULAR_KEY", "Regular key is not set.")]
+        [DataRow("tecNOREGULARKEY", "Regular key is not set.")]
+        [DataRow("TECNO_REGULAR_KEY", "Regular key is not set.")]
+        [DataRow("tecno_regular_key", "Regular key is not set.")]
+        public void TestGetErrorMessage(string key, string message)
+        {
+            Assert.AreEqual(message, Errors.GetErrorMessage(key));
+        }
+    }
+}
